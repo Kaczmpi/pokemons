59156f568bfdd768beb5a486da727d882e2b2ee4
Ozjasz <kaczmarski.piotrr@gmail.com>
2017-06-21 17:36:25 +0200

    Added whole Pokemon list project with tests

---
+++ project/backstop_data/html_report/config.js
@@ -0,0 +1,321 @@
+report({
+  "testSuite": "BackstopJS",
+  "tests": [
+    {
+      "pair": {
+        "reference": "..\\bitmaps_reference\\prod_test_The_Pokemon_List_0_nav_0_phone.png",
+        "test": "..\\bitmaps_test\\20170621-170343\\prod_test_The_Pokemon_List_0_nav_0_phone.png",
+        "selector": "nav",
+        "fileName": "prod_test_The_Pokemon_List_0_nav_0_phone.png",
+        "label": "The Pokemon List",
+        "misMatchThreshold": 0.1,
+        "diff": {
+          "isSameDimensions": true,
+          "dimensionDifference": {
+            "width": 0,
+            "height": 0
+          },
+          "misMatchPercentage": "0.00",
+          "getDiffImage": null
+        }
+      },
+      "status": "pass"
+    },
+    {
+      "pair": {
+        "reference": "..\\bitmaps_reference\\prod_test_The_Pokemon_List_1_bodycol-sm-10_0_phone.png",
+        "test": "..\\bitmaps_test\\20170621-170343\\prod_test_The_Pokemon_List_1_bodycol-sm-10_0_phone.png",
+        "selector": "body .col-sm-10",
+        "fileName": "prod_test_The_Pokemon_List_1_bodycol-sm-10_0_phone.png",
+        "label": "The Pokemon List",
+        "misMatchThreshold": 0.1,
+        "diff": {
+          "isSameDimensions": true,
+          "dimensionDifference": {
+            "width": 0,
+            "height": 0
+          },
+          "misMatchPercentage": "0.99",
+          "analysisTime": 20,
+          "getDiffImage": null
+        },
+        "diffImage": "..\\bitmaps_test\\20170621-170343\\failed_diff_prod_test_The_Pokemon_List_1_bodycol-sm-10_0_phone.png"
+      },
+      "status": "fail"
+    },
+    {
+      "pair": {
+        "reference": "..\\bitmaps_reference\\prod_test_The_Pokemon_List_2_bodycol-sm-7_0_phone.png",
+        "test": "..\\bitmaps_test\\20170621-170343\\prod_test_The_Pokemon_List_2_bodycol-sm-7_0_phone.png",
+        "selector": "body .col-sm-7",
+        "fileName": "prod_test_The_Pokemon_List_2_bodycol-sm-7_0_phone.png",
+        "label": "The Pokemon List",
+        "misMatchThreshold": 0.1,
+        "diff": {
+          "isSameDimensions": true,
+          "dimensionDifference": {
+            "width": 0,
+            "height": 0
+          },
+          "misMatchPercentage": "0.91",
+          "analysisTime": 178,
+          "getDiffImage": null
+        },
+        "diffImage": "..\\bitmaps_test\\20170621-170343\\failed_diff_prod_test_The_Pokemon_List_2_bodycol-sm-7_0_phone.png"
+      },
+      "status": "fail"
+    },
+    {
+      "pair": {
+        "reference": "..\\bitmaps_reference\\prod_test_The_Pokemon_List_3_bodycol-sm-3_0_phone.png",
+        "test": "..\\bitmaps_test\\20170621-170343\\prod_test_The_Pokemon_List_3_bodycol-sm-3_0_phone.png",
+        "selector": "body .col-sm-3",
+        "fileName": "prod_test_The_Pokemon_List_3_bodycol-sm-3_0_phone.png",
+        "label": "The Pokemon List",
+        "misMatchThreshold": 0.1,
+        "diff": {
+          "isSameDimensions": false,
+          "dimensionDifference": {
+            "width": 0,
+            "height": -8
+          },
+          "misMatchPercentage": "12.43",
+          "analysisTime": 72,
+          "getDiffImage": null
+        },
+        "diffImage": "..\\bitmaps_test\\20170621-170343\\failed_diff_prod_test_The_Pokemon_List_3_bodycol-sm-3_0_phone.png"
+      },
+      "status": "fail"
+    },
+    {
+      "pair": {
+        "reference": "..\\bitmaps_reference\\prod_test_The_Pokemon_List_4_footer_0_phone.png",
+        "test": "..\\bitmaps_test\\20170621-170343\\prod_test_The_Pokemon_List_4_footer_0_phone.png",
+        "selector": "footer",
+        "fileName": "prod_test_The_Pokemon_List_4_footer_0_phone.png",
+        "label": "The Pokemon List",
+        "misMatchThreshold": 0.1,
+        "diff": {
+          "isSameDimensions": true,
+          "dimensionDifference": {
+            "width": 0,
+            "height": 0
+          },
+          "misMatchPercentage": "0.00",
+          "analysisTime": 3,
+          "getDiffImage": null
+        }
+      },
+      "status": "pass"
+    },
+    {
+      "pair": {
+        "reference": "..\\bitmaps_reference\\prod_test_The_Pokemon_List_0_nav_1_tablet_v.png",
+        "test": "..\\bitmaps_test\\20170621-170343\\prod_test_The_Pokemon_List_0_nav_1_tablet_v.png",
+        "selector": "nav",
+        "fileName": "prod_test_The_Pokemon_List_0_nav_1_tablet_v.png",
+        "label": "The Pokemon List",
+        "misMatchThreshold": 0.1,
+        "diff": {
+          "isSameDimensions": true,
+          "dimensionDifference": {
+            "width": 0,
+            "height": 0
+          },
+          "misMatchPercentage": "0.00",
+          "getDiffImage": null
+        }
+      },
+      "status": "pass"
+    },
+    {
+      "pair": {
+        "reference": "..\\bitmaps_reference\\prod_test_The_Pokemon_List_1_bodycol-sm-10_1_tablet_v.png",
+        "test": "..\\bitmaps_test\\20170621-170343\\prod_test_The_Pokemon_List_1_bodycol-sm-10_1_tablet_v.png",
+        "selector": "body .col-sm-10",
+        "fileName": "prod_test_The_Pokemon_List_1_bodycol-sm-10_1_tablet_v.png",
+        "label": "The Pokemon List",
+        "misMatchThreshold": 0.1,
+        "diff": {
+          "isSameDimensions": true,
+          "dimensionDifference": {
+            "width": 0,
+            "height": 0
+          },
+          "misMatchPercentage": "0.00",
+          "getDiffImage": null
+        }
+      },
+      "status": "pass"
+    },
+    {
+      "pair": {
+        "reference": "..\\bitmaps_reference\\prod_test_The_Pokemon_List_2_bodycol-sm-7_1_tablet_v.png",
+        "test": "..\\bitmaps_test\\20170621-170343\\prod_test_The_Pokemon_List_2_bodycol-sm-7_1_tablet_v.png",
+        "selector": "body .col-sm-7",
+        "fileName": "prod_test_The_Pokemon_List_2_bodycol-sm-7_1_tablet_v.png",
+        "label": "The Pokemon List",
+        "misMatchThreshold": 0.1,
+        "diff": {
+          "isSameDimensions": true,
+          "dimensionDifference": {
+            "width": 0,
+            "height": 0
+          },
+          "misMatchPercentage": "0.13",
+          "analysisTime": 347,
+          "getDiffImage": null
+        },
+        "diffImage": "..\\bitmaps_test\\20170621-170343\\failed_diff_prod_test_The_Pokemon_List_2_bodycol-sm-7_1_tablet_v.png"
+      },
+      "status": "fail"
+    },
+    {
+      "pair": {
+        "reference": "..\\bitmaps_reference\\prod_test_The_Pokemon_List_3_bodycol-sm-3_1_tablet_v.png",
+        "test": "..\\bitmaps_test\\20170621-170343\\prod_test_The_Pokemon_List_3_bodycol-sm-3_1_tablet_v.png",
+        "selector": "body .col-sm-3",
+        "fileName": "prod_test_The_Pokemon_List_3_bodycol-sm-3_1_tablet_v.png",
+        "label": "The Pokemon List",
+        "misMatchThreshold": 0.1,
+        "diff": {
+          "isSameDimensions": false,
+          "dimensionDifference": {
+            "width": 0,
+            "height": -8
+          },
+          "misMatchPercentage": "9.95",
+          "analysisTime": 115,
+          "getDiffImage": null
+        },
+        "diffImage": "..\\bitmaps_test\\20170621-170343\\failed_diff_prod_test_The_Pokemon_List_3_bodycol-sm-3_1_tablet_v.png"
+      },
+      "status": "fail"
+    },
+    {
+      "pair": {
+        "reference": "..\\bitmaps_reference\\prod_test_The_Pokemon_List_4_footer_1_tablet_v.png",
+        "test": "..\\bitmaps_test\\20170621-170343\\prod_test_The_Pokemon_List_4_footer_1_tablet_v.png",
+        "selector": "footer",
+        "fileName": "prod_test_The_Pokemon_List_4_footer_1_tablet_v.png",
+        "label": "The Pokemon List",
+        "misMatchThreshold": 0.1,
+        "diff": {
+          "isSameDimensions": true,
+          "dimensionDifference": {
+            "width": 0,
+            "height": 0
+          },
+          "misMatchPercentage": "0.00",
+          "analysisTime": 7,
+          "getDiffImage": null
+        }
+      },
+      "status": "pass"
+    },
+    {
+      "pair": {
+        "reference": "..\\bitmaps_reference\\prod_test_The_Pokemon_List_0_nav_2_tablet_h.png",
+        "test": "..\\bitmaps_test\\20170621-170343\\prod_test_The_Pokemon_List_0_nav_2_tablet_h.png",
+        "selector": "nav",
+        "fileName": "prod_test_The_Pokemon_List_0_nav_2_tablet_h.png",
+        "label": "The Pokemon List",
+        "misMatchThreshold": 0.1,
+        "diff": {
+          "isSameDimensions": true,
+          "dimensionDifference": {
+            "width": 0,
+            "height": 0
+          },
+          "misMatchPercentage": "0.00",
+          "getDiffImage": null
+        }
+      },
+      "status": "pass"
+    },
+    {
+      "pair": {
+        "reference": "..\\bitmaps_reference\\prod_test_The_Pokemon_List_1_bodycol-sm-10_2_tablet_h.png",
+        "test": "..\\bitmaps_test\\20170621-170343\\prod_test_The_Pokemon_List_1_bodycol-sm-10_2_tablet_h.png",
+        "selector": "body .col-sm-10",
+        "fileName": "prod_test_The_Pokemon_List_1_bodycol-sm-10_2_tablet_h.png",
+        "label": "The Pokemon List",
+        "misMatchThreshold": 0.1,
+        "diff": {
+          "isSameDimensions": true,
+          "dimensionDifference": {
+            "width": 0,
+            "height": 0
+          },
+          "misMatchPercentage": "0.00",
+          "getDiffImage": null
+        }
+      },
+      "status": "pass"
+    },
+    {
+      "pair": {
+        "reference": "..\\bitmaps_reference\\prod_test_The_Pokemon_List_2_bodycol-sm-7_2_tablet_h.png",
+        "test": "..\\bitmaps_test\\20170621-170343\\prod_test_The_Pokemon_List_2_bodycol-sm-7_2_tablet_h.png",
+        "selector": "body .col-sm-7",
+        "fileName": "prod_test_The_Pokemon_List_2_bodycol-sm-7_2_tablet_h.png",
+        "label": "The Pokemon List",
+        "misMatchThreshold": 0.1,
+        "diff": {
+          "isSameDimensions": true,
+          "dimensionDifference": {
+            "width": 0,
+            "height": 0
+          },
+          "misMatchPercentage": "0.13",
+          "analysisTime": 320,
+          "getDiffImage": null
+        },
+        "diffImage": "..\\bitmaps_test\\20170621-170343\\failed_diff_prod_test_The_Pokemon_List_2_bodycol-sm-7_2_tablet_h.png"
+      },
+      "status": "fail"
+    },
+    {
+      "pair": {
+        "reference": "..\\bitmaps_reference\\prod_test_The_Pokemon_List_3_bodycol-sm-3_2_tablet_h.png",
+        "test": "..\\bitmaps_test\\20170621-170343\\prod_test_The_Pokemon_List_3_bodycol-sm-3_2_tablet_h.png",
+        "selector": "body .col-sm-3",
+        "fileName": "prod_test_The_Pokemon_List_3_bodycol-sm-3_2_tablet_h.png",
+        "label": "The Pokemon List",
+        "misMatchThreshold": 0.1,
+        "diff": {
+          "isSameDimensions": false,
+          "dimensionDifference": {
+            "width": 0,
+            "height": -8
+          },
+          "misMatchPercentage": "15.18",
+          "analysisTime": 37,
+          "getDiffImage": null
+        },
+        "diffImage": "..\\bitmaps_test\\20170621-170343\\failed_diff_prod_test_The_Pokemon_List_3_bodycol-sm-3_2_tablet_h.png"
+      },
+      "status": "fail"
+    },
+    {
+      "pair": {
+        "reference": "..\\bitmaps_reference\\prod_test_The_Pokemon_List_4_footer_2_tablet_h.png",
+        "test": "..\\bitmaps_test\\20170621-170343\\prod_test_The_Pokemon_List_4_footer_2_tablet_h.png",
+        "selector": "footer",
+        "fileName": "prod_test_The_Pokemon_List_4_footer_2_tablet_h.png",
+        "label": "The Pokemon List",
+        "misMatchThreshold": 0.1,
+        "diff": {
+          "isSameDimensions": true,
+          "dimensionDifference": {
+            "width": 0,
+            "height": 0
+          },
+          "misMatchPercentage": "0.00",
+          "getDiffImage": null
+        }
+      },
+      "status": "pass"
+    }
+  ]
+});
